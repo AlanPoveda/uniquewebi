f353d037e2f53bce0bb54944d1229691f303c37a
Alan Poveda <alsepoveda@gmail.com>
2022-10-12 23:18:30 -0300

    Header first configuration

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
       return (
+               <header className="container bg-slate-900">
               <h1 className="text-3xl font-bold underline">
                 Uniquewebi
               </h1>
+                </header>
             )
 }
